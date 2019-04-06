d9c072b18792510574dcd816de1498e01ead0d5b
Harre <34799649+Harreboy@users.noreply.github.com>
2019-04-06 21:26:16 +0200

    First commit
    web view shows in app

---
+++ app/src/main/java/com/example/webview/MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.webview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        webView = (WebView)findViewById(R.id.webView);
+
+        webView.setWebViewClient(new WebViewClient());
+        webView.getSettings().setJavaScriptEnabled(true);
+        webView.getSettings().setDomStorageEnabled(true);
+        webView.setOverScrollMode(WebView.OVER_SCROLL_NEVER);
+        webView.loadUrl("https://www.google.com");
+    }
+}
